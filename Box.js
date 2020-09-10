73182bf34d2a6b23fce80ae65efa90cfb53105ba
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2020-09-10 16:20:01 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,35 @@
+class Box extends BaseClass{
+    constructor(x,y){
+        super(x,y,30,30);
+        rect(x,y,30,30);
+        this.visibility = 255;
+}
+display(){
+    if(this.body.speed<3){
+        var pos = this.body.position
+        var angle = this.body.angle
+        push();
+       translate(pos.x,pos.y)
+       rotate(angle)
+       fill("blue")
+        rectMode(CENTER)
+        rect(0,0,30,30);
+        pop();
+    }else{
+        World.remove(world,this.body);
+         
+        push();
+        this.visibility = this.visibility-5;
+        pop();
+    }
+
+}
+score(){
+    if (this.Visiblity < 0 && this.Visiblity > -1005){
+      score++;
+    }
+  }
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project32
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
